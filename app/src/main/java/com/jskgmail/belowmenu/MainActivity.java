a09e1547910d1ba14323fb149b5cdc37a85b8084
iamjaspreetsingh <jsk1961998@gmail.com>
2017-10-09 23:03:15 +0530

    ok

--- app/src/main/java/com/jskgmail/belowmenu/MainActivity.java
+++ app/src/main/java/com/jskgmail/belowmenu/MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
-    
+
 void go()
     {
 
