f766edaa7250774609137bd065e981e345ae26af
iamjaspreetsingh <jsk1961998@gmail.com>
2017-10-09 22:31:14 +0530

    notify

---
+++ app/src/main/java/com/jskgmail/belowmenu/MyFirebaseInstanceIDService.java
@@ -0,0 +1,35 @@
+package com.jskgmail.belowmenu;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by JASPREET SINGH on 09-10-2017.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("yo", "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String refreshedToken) {
+
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/jskgmail/belowmenu/MyFirebaseMessagingService.java
@@ -0,0 +1,20 @@
+package com.jskgmail.belowmenu;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by JASPREET SINGH on 09-10-2017.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage message)
+    {
+
+    }
+
+
+
+}
