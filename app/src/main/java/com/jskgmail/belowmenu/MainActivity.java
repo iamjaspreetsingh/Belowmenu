402662bcae59cb86bf951890ec99b68e9a402836
iamjaspreetsingh <jsk1961998@gmail.com>
2017-10-09 22:56:22 +0530

    check

--- app/src/main/java/com/jskgmail/belowmenu/MainActivity.java
+++ app/src/main/java/com/jskgmail/belowmenu/MainActivity.java
@@ -14,6 +14,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 import java.util.ArrayList;
 
@@ -72,6 +73,16 @@ public class MainActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+
+    }
 void go()
     {
 
